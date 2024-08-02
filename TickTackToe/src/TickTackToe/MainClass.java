84313bad5681144e7679abafc65725f7ef083523
Munna <munna.syc@gmail.com>
2024-08-02 15:30:59 +0600

    Working on AI

--- TickTackToe/src/TickTackToe/MainClass.java
+++ TickTackToe/src/TickTackToe/MainClass.java
@@ -4,10 +4,12 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		//ChildClass Tick = new ChildClass();
-		//multiPlayer player = new multiPlayer();
-		AIclass AI = new AIclass();
-
+		//ChildClass Tick = new ChildClass(); // This is For testing Purposes
+		
+		
+		//multiPlayer player = new multiPlayer(); /// This is for Playing 2 person
+		
+		AIclass AI = new AIclass(); ///This is to play With AI
 	}
 
 }
