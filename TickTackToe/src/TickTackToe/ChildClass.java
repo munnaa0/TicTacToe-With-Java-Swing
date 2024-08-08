bd42227b7c3a31f5fc97ea5dddea94661a356855
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-08 09:59:44 +0600

    Update ChildClass.java

--- TickTackToe/src/TickTackToe/ChildClass.java
+++ TickTackToe/src/TickTackToe/ChildClass.java
@@ -9,6 +9,12 @@ public class ChildClass extends JFrame implements ActionListener{
 	
 	
 	ChildClass(){
+		
+		///ImageIcon
+		ImageIcon icon = new ImageIcon("icon.png")
+		
+		///For Frame
+		this.setTitle("Tic Tac Toe");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 500);
 	}
