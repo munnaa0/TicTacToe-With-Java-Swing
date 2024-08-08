8bfb2bdcb35c3a48d97741f140a8e9aff45f6802
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-08 10:04:03 +0600

    Update ChildClass.java

--- TickTackToe/src/TickTackToe/ChildClass.java
+++ TickTackToe/src/TickTackToe/ChildClass.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class ChildClass extends JFrame implements ActionListener{
@@ -14,9 +15,17 @@ public class ChildClass extends JFrame implements ActionListener{
 		///ImageIcon
 		ImageIcon icon = new ImageIcon("icon.png");
 		
+		///Buttons
+		JButton mulButton = new JButton();
+		
+		
+		///Adding to frame
+		this.add(mulButton);
+		
 		///For Frame
 		this.setTitle("Tic Tac Toe");
 		this.setIconImage(icon.getImage());
+		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 500);
 		this.setVisible(true);
