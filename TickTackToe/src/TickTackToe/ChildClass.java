aaa6f33f792304183f56c7d8589b37f5d0053185
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2024-08-08 09:57:05 +0600

    Update ChildClass.java

--- TickTackToe/src/TickTackToe/ChildClass.java
+++ TickTackToe/src/TickTackToe/ChildClass.java
@@ -1,9 +1,16 @@
 package TickTackToe;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
 public class ChildClass extends JFrame implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
